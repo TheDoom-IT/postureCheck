79f4c6d19c2e5daf72aefbe7a0115c4c089255d2
TheDoom17 <56804600+TheDoom17@users.noreply.github.com>
2021-01-06 18:26:18 +0100

    Implementation of Darken screen button

--- lib/main.dart
+++ lib/main.dart
@@ -1,180 +1,268 @@
 import 'dart:async';
 import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
+import 'package:screen/screen.dart';
 
 void main() {
-  runApp(
-      MaterialApp(
-        home: MyApp(),
-        title: 'My app',
-      )
-  );
+  runApp(MaterialApp(
+    home: MyApp(),
+    title: 'My app',
+  ));
 }
 
 class MyClock extends StatelessWidget {
-
   final int seconds;
-  final int minutes;
+  final VoidCallback addButton;
+  final VoidCallback minusButton;
+  final countActive;
+  final myText;
 
-  MyClock({this.minutes,this.seconds});
+  MyClock({this.seconds, this.addButton, this.minusButton, this.countActive})
+      : myText = ((seconds / 60).floor() < 10 ? '0' : '') +
+            (seconds / 60).floor().toString() +
+            ':' +
+            (seconds % 60 < 10 ? '0' : '') +
+            (seconds % 60).toString();
 
   @override
-  Widget build(BuildContext context){
-    return Column(
-        children: <Widget>[
-          Expanded(
-            child: Container(),
-          ),
-          Expanded(
-              child: Container(
-                width: MediaQuery.of(context).size.width/4.0*3,
-                color: Colors.lightGreen,
-                child: Text(minutes.toString() + ' ' + seconds.toString()),
-              )
-          ),
-          Expanded(
-            child: Container(),
-          ),
-        ]
+  Widget build(BuildContext context) {
+    //shows only minutes and buttons to change them
+    if (countActive == false) {
+      return Column(children: <Widget>[
+        Expanded(
+          child: Container(),
+        ),
+        Expanded(
+            flex: 2,
+            child: Container(
+                child: Column(children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.blueGrey,
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                      color: Colors.white60,
+                    width: 2,
+                  ),
+                ),
+                child: IconButton(
+                  icon: Icon(Icons.add),
+                  color: Colors.white60,
+                  onPressed: addButton,
+                ),
+              ),
+              Expanded(
+                child: Center(
+                  child: Text(
+                    (seconds / 60).floor().toString(),
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 100,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.blueGrey,
+                  border: Border.all(
+                    color: Colors.white60,
+                    width: 2,
+                  ),
+                ),
+                child: IconButton(
+                  icon: Icon(Icons.remove),
+                  color: Colors.white60,
+                  onPressed: minusButton,
+                ),
+              ),
+            ]))),
+        Expanded(
+          child: Container(),
+        ),
+      ]);
+    }
+    return Center(
+      child: Text(
+        myText,
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 100,
+        ),
+      ),
     );
   }
 }
 
-class MyButton extends StatelessWidget{
-
-  MyButton({this.buttonString,this.onPressed});
+class MyButton extends StatelessWidget {
+  MyButton({this.buttonString, this.onPressed});
 
   final buttonString;
   final VoidCallback onPressed;
 
-  final buttonColor = Colors.blue;
+  final buttonColor = Colors.white;
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Container(
-      height: 50,
-      width: 150,
-      decoration: BoxDecoration(
-        color: buttonString == "Start" ? null : buttonColor,
-        border: Border.all(
-          color: buttonColor,
-          width: 2,
+        height: 50,
+        width: 150,
+        decoration: BoxDecoration(
+          color: buttonString == "Start" ? null : buttonColor,
+          border: Border.all(
+            color: buttonColor,
+            width: 2,
+          ),
         ),
-      ),
-      child: FlatButton(
-        child: Text(buttonString),
-        onPressed: onPressed,
-      )
+        child: FlatButton(
+          child: Text(buttonString),
+          onPressed: onPressed,
+        ));
+  }
+}
+
+class MySwitch extends StatelessWidget {
+  MySwitch({this.darkenScreen, this.onChanged});
+
+  final bool darkenScreen;
+  final void Function(bool) onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 80,
+      child: Column(children: [
+        Expanded(child: Container()),
+        Switch(
+          activeColor: Colors.white,
+          value: darkenScreen,
+          onChanged: onChanged,
+        ),
+        Text('Darken screen',
+            style: TextStyle(
+              color: Colors.white70,
+            ))
+      ]),
     );
   }
 }
 
-class MyApp extends StatefulWidget{
+class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp>{
-
+class _MyAppState extends State<MyApp> {
   Timer timer;
-  _MyAppState(){
-    timer = new Timer.periodic(new Duration(seconds: 1), _decrement);
+  double brightnessSaved;
+  int secondsToCount = 60;
+  int minutesSet = 1;
+  bool countActive = false;
+  bool darkenScreen = false;
+  AudioCache audio = AudioCache();
+  var buttonString = "Start";
+
+  _MyAppState() {
+    getBrightness();
+    timer = new Timer.periodic(new Duration(seconds: 1), decrementTimer);
     audio.load('sound.wav');
   }
-  var buttonString = "Start";
-  int secondsCurr = 5;
-  int minutesCurr = 0;
-  int secondsSaved = 5;
-  int minutesSaved = 0;
-  bool countActive = false;
-  AudioCache audio = AudioCache();
 
-  bool isCountEnd()
-  {
-    return secondsCurr == 0 && minutesCurr == 0;
+  void getBrightness() async {
+    brightnessSaved = await Screen.brightness;
+  }
+
+  void changeDarken(bool value) {
+    setState(() {
+      darkenScreen = value;
+    });
   }
 
   void playSound() {
     audio.play('sound.wav');
   }
-  
-  void resetTime(){
-    secondsCurr = secondsSaved;
-    minutesCurr = minutesSaved;
+
+  void setTime() {
+    secondsToCount = minutesSet * 60;
   }
 
-  void startStopCounter(){
+  void startStopCounter() {
     setState(() {
       countActive = !countActive;
       buttonString = buttonString == "Start" ? "Stop" : "Start";
+      setTime();
 
-      if(countActive) {
-          secondsSaved = secondsCurr;
-          minutesSaved = minutesCurr;
+      if (countActive) {
+        Screen.keepOn(true);
+
+        if (darkenScreen) {
+          Screen.setBrightness(0);
         }
+      } else {
+        Screen.keepOn(false);
+        Screen.setBrightness(brightnessSaved);
+      }
     });
   }
 
-  void _decrement(Timer timer) {
-    if(countActive) {
-      secondsCurr--;
-      if(isCountEnd()) {
-          playSound();
-          resetTime();
-        }
-
+  void decrementTimer(Timer timer) {
+    if (countActive) {
+      secondsToCount--;
+      if (secondsToCount == 0) {
+        playSound();
+        setTime();
+      }
       setState(() {});
     }
+  }
+
+  void addMinute() {
+    minutesSet++;
+    if (minutesSet > 59) {
+      minutesSet = 1;
     }
+    setTime();
+    setState(() {});
+  }
 
-
+  void substractMinute() {
+    minutesSet--;
+    if (minutesSet < 1) {
+      minutesSet = 59;
+    }
+    setTime();
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.deepOrange,
-          title: Center(
-            child: Text(secondsCurr.toString()),
+        backgroundColor: Colors.blueGrey,
+        body: Column(children: <Widget>[
+          //Clock part
+          Expanded(
+            flex: 3,
+            child: MyClock(
+              seconds: secondsToCount,
+              addButton: addMinute,
+              minusButton: substractMinute,
+              countActive: countActive,
             ),
           ),
-        body: Center(
-            child: Column(
-                children: <Widget>[
-                  //Clock part
-                  Expanded(
-                    flex: 4,
-                    child: MyClock(seconds: secondsCurr,minutes: minutesCurr,),
-                  ),
-                  //Button part
-                  Expanded(
-                    flex: 1,
-                      child: Column(
-                        children: <Widget>[
-                          Expanded(
-                            child: Container(color: Colors.red,),
-                          ),
-                           Container(
-                             height: 50,
-                             width: 150,
-                             child: MyButton(
-                               buttonString: buttonString,
-                               onPressed: startStopCounter,
-                             ),
-                           ),
-                          Expanded(
-                            child: Container(color: Colors.red,),
-                          )
-
-                        ]
-                      ),
-                  ),
-                ]
-            )
-        ),
+          //Button part
+          Expanded(
+            child: Column(children: [
+              MyButton(
+                buttonString: buttonString,
+                onPressed: startStopCounter,
+              ),
+              MySwitch(darkenScreen: darkenScreen, onChanged: changeDarken)
+            ]),
+          ),
+        ]),
       ),
     );
   }
 }
-
